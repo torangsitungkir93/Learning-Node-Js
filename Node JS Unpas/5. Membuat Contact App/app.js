74c6cb8465874b025bb479bc0900ae02eab04054
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-02-03 23:52:32 +0700

    fix bug

--- Node JS Unpas/5. Membuat Contact App/app.js
+++ Node JS Unpas/5. Membuat Contact App/app.js
@@ -1,22 +1,13 @@
 
-
-
+const contacts = require('./contacts');
 
 
 const main = async () => {
-    const nama = await tulisPertanyaan('Masukkan Nama Anda : ');
-    const email = await tulisPertanyaan('Masukkan Email Anda : ');
-    const noHP = await tulisPertanyaan('Masukkan NoHP Anda : ');
+    const nama = await contacts.tulisPertanyaan('Masukkan Nama Anda : ');
+    const email = await contacts.tulisPertanyaan('Masukkan Email Anda : ');
+    const noHP = await contacts.tulisPertanyaan('Masukkan NoHP Anda : ');
 
-    const contact = {nama,email,noHP};
-    const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
-    const contacts = JSON.parse(fileBuffer);
-
-    contacts.push(contact);
-    fs.writeFileSync('data/contacts.json',JSON.stringify(contacts));
-
-    console.log('Terimakasih sudah memasukkan data.');
-    rl.close();
+   contacts.simpanContact(nama,email,noHP);
 };
 
 main();
--- Node JS Unpas/5. Membuat Contact App/contacts.js
+++ Node JS Unpas/5. Membuat Contact App/contacts.js
@@ -1,10 +1,26 @@
 
+const fs = require(`fs`);
+const readline = require('readline');
 
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+// Membuat folder Data
+const dirPath = "./data";
+if(!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath);
+}
+
+// Membuat file contacts.json jika belum ada
 const dataPath = './data/contacts.json';
 if(!fs.existsSync(dataPath)){ 
     fs.writeFileSync(dataPath,'[]','utf-8');
     console.log('Terdapat bug');
 }
+
+// Bungkus kedalam Promise
 
 const tulisPertanyaan = (pertanyaan) => {
     return new Promise((resolve,reject) =>{
@@ -14,7 +30,8 @@ const tulisPertanyaan = (pertanyaan) => {
     });
 }
 
-const simpanContact = () => {
+
+const simpanContact = (nama,email,noHP) => {
     const contact = {nama,email,noHP};
     const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
     const contacts = JSON.parse(fileBuffer);
@@ -25,4 +42,6 @@ const simpanContact = () => {
     console.log('Terimakasih sudah memasukkan data.');
     rl.close();
 };
+
+module.exports = {tulisPertanyaan,simpanContact};
 
